845173bd1cc292a7c442e8438f5c63eefbf5903b
youngjunkim <kyj0703@konkuk.ac.kr>
2023-11-13 16:13:32 +0900

    test: 평일할인 관련 테스트 코드 추가

--- src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
+++ src/test/java/christmas/domain/discount/WeekdayDiscountTest.java
@@ -10,56 +10,60 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class SpecialDiscountTest {
+class WeekdayDiscountTest {
     private EventDate eventDate;
     private int beforeDiscountPrice;
+    private int menuCount;
 
     @BeforeEach
     void beforeEach() {
         eventDate = new EventDate(3);
         beforeDiscountPrice = 10000;
+        menuCount = 2;
     }
 
     @Test
     void calculate() {
-        Discount discount = new SpecialDiscount(eventDate, beforeDiscountPrice);
-        Assertions.assertThat(discount.calculate()).isEqualTo(1000);
+        Discount discount = new WeekdayDiscount(eventDate, menuCount, beforeDiscountPrice);
+
+        Assertions.assertThat(discount.calculate()).isEqualTo(2023*2);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 10, 17, 24, 31})
-    @DisplayName("특별할인은 일요일에 적용됩니다.")
+    @ValueSource(ints = {3,4,5,6,7})
+    @DisplayName("평일할인은 일,월,화,수,목에 적용됩니다.")
     void checkSundayTest(int day) {
         eventDate = new EventDate(day);
-        Discount discount = new SpecialDiscount(eventDate, beforeDiscountPrice);
+        Discount discount = new WeekdayDiscount(eventDate, menuCount, beforeDiscountPrice);
 
         assertTrue(discount.canDiscount());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,4,5,6,7})
-    @DisplayName("특별할인은 일요일이 아니면 적용되지 않습니다.")
+    @ValueSource(ints = {8,9})
+    @DisplayName("평일할인은 금,토에는 적용되지 않습니다.")
     void checkNotSundayTest(int day) {
         eventDate = new EventDate(day);
-        Discount discount = new SpecialDiscount(eventDate, beforeDiscountPrice);
+        Discount discount = new WeekdayDiscount(eventDate, menuCount, beforeDiscountPrice);
 
         assertFalse(discount.canDiscount());
     }
 
-    @Test
-    @DisplayName("특별할인은 크리스마스에 적용됩니다.")
-    void checkXmasTest() {
-        eventDate = new EventDate(25);
-        Discount discount = new SpecialDiscount(eventDate, beforeDiscountPrice);
+    @ParameterizedTest
+    @ValueSource(ints = {0,-1})
+    @DisplayName("평일할인은 주문개수가 0개 이하면 적용되지 않습니다.")
+    void checkXmasTest(int count) {
+        menuCount = count;
+        Discount discount = new WeekdayDiscount(eventDate, menuCount, beforeDiscountPrice);
 
-        assertTrue(discount.canDiscount());
+        assertFalse(discount.canDiscount());
     }
 
     @Test
     @DisplayName("할인 전 금액이 10000원 이상이여야 혜택이 적용됩니다.")
     void eventTriggerTest() {
         beforeDiscountPrice = 9999;
-        Discount discount = new SpecialDiscount(eventDate, beforeDiscountPrice);
+        Discount discount = new WeekdayDiscount(eventDate, menuCount, beforeDiscountPrice);
 
         assertFalse(discount.canDiscount());
     }
