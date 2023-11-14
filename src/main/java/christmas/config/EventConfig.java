e0c6841d45fd13aeb2af07bd94ae09837ea09520
youngjunkim <kyj0703@konkuk.ac.kr>
2023-11-14 17:26:40 +0900

    feat: event 계산 로직 분리

--- src/main/java/christmas/config/EventConfig.java
+++ src/main/java/christmas/config/EventConfig.java
@@ -3,6 +3,7 @@ package christmas.config;
 import christmas.controller.EventController;
 import christmas.domain.Order;
 import christmas.service.DiscountService;
+import christmas.service.EventCalculator;
 import christmas.service.OrderService;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -11,19 +12,23 @@ public class EventConfig {
     private static Order order;
 
     public static EventController getEventController() {
-        return new EventController(getInputView(),getOutputView(),getOrderService(),getDiscountService());
+        return new EventController(getInputView(), getOutputView(), getEventCalculator());
     }
 
-    private static DiscountService getDiscountService() {
-        return new DiscountService(getOrder());
+    private static EventCalculator getEventCalculator() {
+        return new EventCalculator(getOrderService(), getDiscountService());
     }
 
     private static OrderService getOrderService() {
         return new OrderService(getOrder());
     }
 
+    private static DiscountService getDiscountService() {
+        return new DiscountService(getOrder());
+    }
+
     private static Order getOrder() {
-        if(order != null){
+        if (order != null) {
             return order;
         }
         return new Order();
---
+++ src/main/java/christmas/service/EventCalculator.java
@@ -0,0 +1,84 @@
+package christmas.service;
+
+import christmas.domain.EventDate;
+import christmas.domain.discount.DdayDiscount;
+import christmas.domain.discount.Discount;
+import christmas.domain.discount.GiveawayDiscount;
+import christmas.domain.discount.SpecialDiscount;
+import christmas.domain.discount.WeekdayDiscount;
+import christmas.domain.discount.WeekendDiscount;
+import java.util.List;
+
+public class EventCalculator {
+    private OrderService orderService;
+    private DiscountService discountService;
+    private String orderMenu;
+    private int beforeDiscountPrice;
+    private String giveawayMenu;
+    private List<Discount> totalDiscounts;
+    private int totalDiscountPrice;
+    private int totalDiscountPriceExpectGiveaway;
+    private String badge;
+
+    public EventCalculator(OrderService orderService, DiscountService discountService) {
+        this.orderService = orderService;
+        this.discountService = discountService;
+    }
+
+    public void calculateDiscounts(EventDate eventDate) {
+        orderMenu = orderService.printOrder();
+        beforeDiscountPrice = orderService.getPriceBeforeDiscount();
+        setDiscounts(eventDate);
+        giveawayMenu = discountService.calculateGiveaway();
+        totalDiscounts = discountService.getTotalDiscounts();
+        totalDiscountPrice = discountService.calculateTotalDiscountPrice();
+        totalDiscountPriceExpectGiveaway = discountService.calculateTotalDiscountExceptGiveaway();
+        badge = orderService.getBadge(totalDiscountPrice);
+    }
+
+    public String getOrderMenu() {
+        return orderMenu;
+    }
+
+    public int getBeforeDiscountPrice() {
+        return beforeDiscountPrice;
+    }
+
+    public String getGiveawayMenu() {
+        return giveawayMenu;
+    }
+
+    public List<Discount> getTotalDiscounts() {
+        return totalDiscounts;
+    }
+
+    public int getTotalDiscountPrice() {
+        return totalDiscountPrice;
+    }
+
+    public int getTotalDiscountPriceExpectGiveaway() {
+        return totalDiscountPriceExpectGiveaway;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    private void setDiscounts(EventDate eventDate) {
+        List<Discount> discounts = getDiscounts(eventDate);
+
+        for (Discount discount : discounts) {
+            discountService.setDiscount(discount);
+        }
+    }
+
+    private List<Discount> getDiscounts(EventDate eventDate) {
+        return List.of(
+                new DdayDiscount(eventDate,beforeDiscountPrice),
+                new WeekdayDiscount(eventDate, orderService.getCountOfDesserts(),beforeDiscountPrice),
+                new WeekendDiscount(eventDate, orderService.getCountOfMainMenus(),beforeDiscountPrice),
+                new SpecialDiscount(eventDate,beforeDiscountPrice),
+                new GiveawayDiscount(orderService.getPriceBeforeDiscount())
+        );
+    }
+}
